6ea5fa958c84cdb25c7dac40e07842e29991f05e
Eric Ekholm <eric.ekholm@gmail.com>
2021-01-26 21:33:31 -0500

    init commit

---
+++ R/release_dogs.R
@@ -0,0 +1,10 @@
+
+#' Let the Dogs Out!
+#'
+#' Open "Who Let the Dogs Out" in your browser
+#'
+#' @return
+#' @export
+release_dogs <- function() {
+  browseURL("https://www.youtube.com/watch?v=5St2O-9cTx8")
+}
